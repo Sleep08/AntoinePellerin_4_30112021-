0117f3094eef646c3a10dbd14a59d07aaaf0576a
Antoine Pellerin <antoine.pellerin@yahoo.com>
2021-11-30 14:34:57 +0100

    ajout des constantes

---
+++ gameon.js
@@ -0,0 +1,13 @@
+/* CONSTANTES DES CHAMPS */
+const prenom = document.getElementById('first');
+const nom = document.getElementById('last');
+const email = document.getElementById('email');
+const birthDate = document.getElementById('birthdate');
+const nbTournoi = document.getElementById('quantity');
+const choixLocation = document.getElementById('choixlocation');
+const checkLocation = document.getElementsByName('location');
+const boxChoice = document.getElementById('box-choice');
+const btnConfirm = document.getElementById('btn-confirm');
+
+/* VERIFICATION DES ELEMENTS DU FORM */
+
